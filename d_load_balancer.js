77ccd584d92f4999f3c272da723cadcd567055ee
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 12:46:57 -0600

    Update d_load_balancer.js

--- d_load_balancer.js
+++ d_load_balancer.js
@@ -1,3 +1,40 @@
+import {getConfig, setConfig} from "util.js";
+
 export async function main(ns) {
- 
+	const PBL = ns.getPortHandle(1);
+	const PRBTS = ns.getPortHandle(3);
+	const LS = ns.getPortHandle(6);
+	const tSize = 1.75;
+	
+	function sum (a,b) {
+		return a.threads + b.threads;	
+	}
+	function desc(a,b) {
+		return 	b.threads - a.threads;
+	}
+	
+	while(true){
+		const CONFIG = getConfig(ns);
+		const INTERVAL = CONFIG.interval;
+		while(PBL.data[0] != "NULL PORT DATA"){
+		
+			let rootServers = PBL.data[0];
+			rootServers.forEach(rs => {
+				rs.threads = Math.floor((rs.mRam - ns.getServerUsedRam(rs.hostname)) / tSize);
+			}).sort(desc);
+			
+			let down = LS.data[1];
+			let dsi = rootServers.findIndex(fi => fi.hostname == down);
+			if(dsi >= 0) rootServers.splice(dsi, 1);
+			
+			let total = rootServers.reduce(sum, 0);
+			if(total < 1){
+				LS.data[0] = true;	
+			}
+			PRBTS.data[0] = JSON.stringify(rootServers);
+			await ns.sleep(INTERVAL);
+			
+		}
+		await ns.sleep(INTERVAL);
+	}
 }
