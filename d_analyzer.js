5a9439b1e181836d4ae92e2def42095fe225cfac
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-15 18:27:34 -0600

    Update d_analyzer.js

--- d_analyzer.js
+++ d_analyzer.js
@@ -1,3 +1,27 @@
+//import * from "";
+
 export async function main(ns) {
+	const hst = 0.002;
+	const gst = 0.004;
+	const wst = 0.05;
+	//percent increase to get back to 100% max money from 90% max money.
+	const gmul = 1.12;
+	while(true){
+		let priority = JSON.parse(ns.getPortHandle(2).data[0]);
+		let target = priority.hostname;
+		let analysis = {
+			"hostname": target,
+			"hackThreads": 0,
+			"weakThreads1": 0,
+			"growThreads": 0,
+			"weakThreads2": 0,
+			"total": 0 
+		};
 		
+		let threads= [0,0,0,0];
+		//flooring the division make the taken pencentage 10% or just under 10%. never over 10%
+		threads[0] = Math.floor(.1 / ns.hackAnalyze(target));
+		threads[1] = (hst * threads[0]) / wst;
+		threads[2] = Math.ceil(ns.growthAnalyze(target, gmul));
+	}
 }
