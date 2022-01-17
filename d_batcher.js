3a9b4d7264d1c929408380d281cf948dcc06a298
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 20:45:15 -0600

    Update d_batcher.js

--- d_batcher.js
+++ d_batcher.js
@@ -1,3 +1,43 @@
+import {getConfig} from "util.js";
+import {batch} from "u_functions.js";
+
 export async function main(ns) {
- 	 
+	
+	const LBL = ns.getPortHandle(3);
+	const AZL = ns.getPortHandle(4);
+	const PRL = ns.getPortHandle(5);
+	let servers = [];
+	let AZB = "";
+	let ptarget = {};
+	
+	function sum(a,b){
+		return a.threads + b.threads;	
+	}
+	while(true){
+		const CONFIG = getConfig(ns);
+		const INTERVAL = CONFIG.interval;
+		while(LBL.data[0] != "NULL PORT DATA" && AZL.data[0] != "NULL PORT DATA"){
+			let primed = JSON.parse(PRL.data[0]);
+			if(!primed){
+				await ns.sleep(INTERVAL);
+				continue;
+			}
+			ptarget = JSON.parse(AZL.data[0]);
+			AZB = JSON.parse(AZL.data[1]);
+			servers = JSON.parse(LBL.data[0]);
+			
+			let aRam = servers.reduce(sum, 0);	
+			
+			if(aRam < AZB.total) {
+				await ns.sleep(INTERVAL);
+				continue;
+			}
+			else{
+				batch(ns, servers, AZB, ptarget.hostname);
+				await ns.sleep(INTERVAL);
+			}
+			
+		}
+	}
+	
 }
