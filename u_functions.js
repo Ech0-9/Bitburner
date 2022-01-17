b9d5fb3dca27aafc71763c83a3f525f48a3a0fb0
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 22:02:33 -0600

    Update u_functions.js

--- u_functions.js
+++ u_functions.js
@@ -32,9 +32,23 @@ function hgw(ns, ty, s, t, az, tg){
 		ns.exec(SCRIPTS[i], ss.hostname, ss.threads, [tg, d]);
 	}
 }
-export function batch(ns, s, azb, tg){
-			hgw(ns, "HACK", s, azb.hackThreads, azb, tg);
-			hgw(ns, "WEAK", s, azb.h_weakThreads, azb, tg);
-			hgw(ns, "GROW", s, azb.growThreads, azb, tg);
-			hgw(ns, "WEAK", s, azb.g_weakThreads, azb, tg);
-		}
+//i must be zero
+export function batch(ns, s, azb, tg, d, i){
+	hgw(ns, "HACK", s, Math.ceil(azb.hackThreads/d), azb, tg);
+	hgw(ns, "WEAK", s, Math.ceil(azb.h_weakThreads/d), azb, tg);
+	hgw(ns, "GROW", s, Math.ceil(azb.growThreads/d), azb, tg);
+	hgw(ns, "WEAK", s, Math.ceil(azb.g_weakThreads/d), azb, tg);
+	i++;
+	if(i < d){
+		batch(ns, s, azb, tg, d, i);	
+	}
+}
+//i must be zero on initial call
+export function prime(ns, s, azp, tg, d, i){
+	hgw(ns, "GROW", s, Math.ceil(azp.growThreads/d), azp, tg);
+	hgw(ns, "WEAK", s, Math.ceil(azp.g_weakThreads/d), azp, tg);
+	i++;
+	if(i < d){
+		prime(ns, s, azp, tg, d, i);	
+	}
+}
