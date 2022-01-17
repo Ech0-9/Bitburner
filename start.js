ee8883198c0c4f375ff7e5b23828872e28fc08fe
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 23:42:14 -0600

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
-import {getConfig, setConfig} from "util.js";
+import { setConfig } from "util.js";
 
 export async function main(ns){
-		const CONFIG = getConfig(ns);
-		
+	const daemons = ["runLoadBalancer", "runAnalyzer", "runLogistics", "runBatcherLB", "runBatcherAZ", "runPrimerLB", "runPrimerAZ"];
+	daemons.forEach(d => {
+		setConfig(ns, {d: false});	
+	}
+	ns.run("d_central_brain.js", 1);
+	
 }
