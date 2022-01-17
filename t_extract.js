462ccc1c8a8421cc6b6f133813407eb03496d2be
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 21:33:19 -0600

    Update t_extract.js

--- t_extract.js
+++ t_extract.js
@@ -2,5 +2,6 @@
 export async function main(ns){
 	const f = ns.flags([]);
 	await ns.sleep(f._[1]);
-	await ns.hack(f._[0]);
+	let result =  ns.hack(f._[0]);
+	ns.toast("Hack Result on ${_[0]}: ${result}", "success", 3000);
 }
