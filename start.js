fb25e6ada0d511a064f3c31acb2b27ef0db443c1
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-16 23:46:04 -0600

    Update start.js

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ export async function main(ns){
 	const daemons = ["runLoadBalancer", "runAnalyzer", "runLogistics", "runBatcherLB", "runBatcherAZ", "runPrimerLB", "runPrimerAZ"];
 	daemons.forEach(d => {
 		setConfig(ns, {d: false});	
-	}
+	});
 	ns.run("d_central_brain.js", 1);
 	
 }
