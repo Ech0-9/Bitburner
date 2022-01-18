cf3c68437d154364f6d48acbdfe66137bbf004bb
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-18 07:51:37 -0600

    Update d_primer.js

--- d_primer.js
+++ d_primer.js
@@ -12,14 +12,18 @@ export async function main(ns){
 		return a.threads + b.threads;	
 	}
 	while(true){
-		while(LBL.data[0] != "NULL PORT DATA" && AZL.data[0] != "NULL PORT DATA"){
-			const CONFIG = getConfig(ns);
-			const INTERVAL = CONFIG.interval;
+		const CONFIG = getConfig(ns);
+		const INTERVAL = CONFIG.interval;
+		if(LBL.peek() != "NULL PORT DATA" && AZL.peek() != "NULL PORT DATA"){
 			
-			ptarget = JSON.parse(AZL.data[0]);
-			PRS.data[0] = JSON.strigifiy(ptarget.primed);
-			AZP = JSON.parse(AZL.data[2]);
-			servers = JSON.parse(LBL.data[0]);
+			//upates priority target
+			ptarget = JSON.parse(AZL.peek())[0];
+			//sends true or false to probe and batcher
+			PRS.read();
+			PRS.write(JSON.strigifiy(ptarget.primed));
+			//
+			AZP = JSON.parse(AZL.peek())[2];
+			servers = JSON.parse(LBL.peek());
 			let aRam = servers.reduce(sum, 0);
 			if(!ptarget.primed){
 				let div = 0;
@@ -33,15 +37,13 @@ export async function main(ns){
 				else{
 					prime(ns, servers, AZP, ptarget.hostname, Math.ceil(AZP.total/aRam), 0);
 					ptarget.primed = true;
-					PRS.data[0] = JSON.strigifiy(ptarget.primed);
-					/*
-						wait 5 seconds (INTERVAL: 100 times 50) for probe and analyzer to catch up.
-						target primed is sent to batcher and probe
-						probe will update the primed field if the priority has not changed
-						which is then sent to analyzer which is sent back to primer
-						this way primer does not try to prime the target again due to race conditions
-					*/
-					await ns.sleep(INTERVAL * 50);
+					if(PRS.peek() != "NULL PORT DATA"){
+						PRS.read();	
+					}
+					PRS.write(JSON.strigifiy(ptarget.primed));
+					//increased interval to ensure Probe updates the primed field sent to analyzer which is sent to primer
+					//this way we don't try an prime a primed server.
+					await ns.sleep(INTERVAL * 5);
 				}
 			}
 		}
