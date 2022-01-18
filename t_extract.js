54f188d8806200a4307b54f39e5b16d68ea35b9c
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-18 15:25:58 -0600

    Update t_extract.js

--- t_extract.js
+++ t_extract.js
@@ -1,7 +1,11 @@
 //will be the hack script
 export async function main(ns){
-	const f = ns.flags([]);
-	await ns.sleep(f._[1]);
-	let result =  ns.hack(f._[0]);
-	ns.toast("Hack Result on ${_[0]}: ${result}", "success", 3000);
+	const f = ns.flags([
+		["target", ""],
+		["delay", ""],
+		["id", -1]
+	]);
+	await ns.sleep(f.delay);
+	let result =  ns.hack(f.target);
+	ns.toast(`Hack Result on ${f.target}: ${result}`, "success", 3000);
 }
