1e31f445db7fc076d33a58f1cfd264da6f14b5c9
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-18 15:24:23 -0600

    Update t_engorge.js

--- t_engorge.js
+++ t_engorge.js
@@ -1,6 +1,10 @@
 /** @param {NS} ns **/
 export async function main(ns){
-	const f = ns.flags([]);
-	await ns.sleep(f._[1]);
-	await ns.grow(f._[0]);
+	const f = ns.flags([
+		["target", ""],
+		["delay", ""],
+		["id", -1]
+	]);
+	await ns.sleep(f.delay);
+	await ns.grow(f.target);
 }
