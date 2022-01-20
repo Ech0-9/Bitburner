c67f54a3d7337de4e39dac09b53035663bbc0c29
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-20 07:53:26 -0600

    Update d_central_brain.js

--- d_central_brain.js
+++ d_central_brain.js
@@ -2,30 +2,26 @@ import {getConfig} from "util.js";
 
 export async function main(ns){
 	const ME = ns.getHostname();	
-	const SCRIPTS = ["d_probe.js", "d_load_balancer.js", "d_analyzer.js", "d_batcher.js", "d_primer.js", "d_logistics.js"];
+	const SCRIPTS = ["d_probe.js", "d_batcher.js", "d_primer.js"];
 	while(true){
 		const CONFIG = getConfig(ns);
 		const INTERVAL = CONFIG.interval;
 		//checks if Probe should/n't be running
 		const SERVICE = [
-			[CONFIG.runProbe, true, "d_probe.js"],
-			[CONFIG.runLoadBalancer, true, "d_load_balancer.js"],
-			[CONFIG.runAnalyzer, true, "d_analyzer.js"],
-			[CONFIG.runBatcherLB, CONFIG.runBatcherAZ, "d_batcher.js"],
-			[CONFIG.runPrimerLB, CONFIG.runPrimerAZ, "d_primer.js"],
-			[CONFIG.runLogisticsST, true, "d_logistics.js"]
+			[CONFIG.runProbe, "d_probe.js"],
+			[CONFIG.runBatcher, "d_batcher.js"],
+			[CONFIG.runPrimer, "d_primer.js"]
 		];
 		for(let i = 0; i < SERVICE.length; i++){
 			let a = SERVICE[i][0];
-			let b = SERVICE[i][1];
-			let s = SERVICE[i][2];
+			let s = SERVICE[i][1];
 			if(ns.scriptRunning(s, ME)){
-				if(!a || !b){
+				if(!a){
 					ns.scriptKill(s, ME);
 				}
 			}
 			else {
-				if(a && b){
+				if(a){
 					ns.run(s, 1);
 				}
 			}
