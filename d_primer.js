fd1f55452894526a931c577f22de6e5339b1bd6b
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-20 08:52:05 -0600

    Update d_primer.js

--- d_primer.js
+++ d_primer.js
@@ -1,14 +1,14 @@
 import {getConfig} from "util.js";
-import {prime} from "u_functions.js";
+import {prime, analyze, scriptRunningAny} from "u_functions.js";
 
 export async function main(ns){	
-	const LBL = ns.getPortHandle(3);
-	const AZL = ns.getPortHandle(4);
-	const PRS = ns.getPortHandle(5);
+	const port1 = ns.getPortHandle(1);
+	const port2 = ns.getPortHandle(2);
+	const port3 = ns.getPortHandle(3);
 	let oldPriority = {
 		"hostname": "NOT A SERVER"
 	};
-	let servers = [];
+	let cluster = [];
 	let AZP = {};
 	let ptarget = {};
 	function sum(a,b){
@@ -17,44 +17,52 @@ export async function main(ns){
 	while(true){
 		const CONFIG = getConfig(ns);
 		const INTERVAL = CONFIG.interval;
-		if(LBL.peek() != "NULL PORT DATA" && AZL.peek() != "NULL PORT DATA"){
+		let div = 0;
+		if(port1.peek() != "NULL PORT DATA" && port2.peek() != "NULL PORT DATA"){
 			
 			//upates priority target
-			ptarget = JSON.parse(AZL.peek())[0];
+			ptarget = JSON.parse(port2.peek());
 			//sends true or false to probe and batcher
-			if(PRS.peek() == "NULL PORT DATA"){
-				PRS.write(JSON.stringify(ptarget));
+			if(port3.peek() == "NULL PORT DATA"){
+				port3.write(JSON.stringify(ptarget));
 			}
 			if(ptarget.hostname != oldPriority.hostname){
-				PRS.read();
-				PRS.write(JSON.stringify(ptarget);
+				port3.write(JSON.stringify(ptarget));
+				port3.read();
 			}
 			oldPriority = ptarget;
 			//
-			AZP = JSON.parse(AZL.peek())[2];
-			servers = JSON.parse(LBL.peek());
+			AZP = analyze(ns, ptarget, "P");
+			cluster = JSON.parse(port1.peek());
 			//await ns.write("LOGSERVERS.txt", JSON.stringify(servers), "w");
 			//let aRam = servers.reduce(sum, 0);
 			let aRam = 0;
-			for(let i = 0; i < servers.length; i++){
-				aRam += servers[i].threads;	
+			for(let i = 0; i < cluster.length; i++){
+				aRam += cluster[i].threads;	
 			}
 			if(!ptarget.primed){
-				let div = 0;
 				if(aRam != 0){
 					div = Math.ceil(AZP.total/aRam);	
 				}
-				if(div > 5 || div <= 0){
+				if(div <= 0){
 					await ns.sleep(INTERVAL);
 					continue;
 				}
 				else{
-					prime(ns, servers, AZP, ptarget.hostname, div, 0);
-					ptarget.primed = true;
-					if(PRS.peek() != "NULL PORT DATA"){
-						PRS.read();	
+					//wait until all batches clear out
+					if(scriptRunningAny(ns, "t_enfeeble.js", cluster)){
+						await ns.sleep(INTERVAL * 10);
+						//need to quit out of this loop and see if ptarget has changed
+						continue;
 					}
-					PRS.write(JSON.stringify(ptarget.primed));
+					//all ram available for priming and ptarget updated
+					for(let i = 0,j = 0,s = 0; i < div; i++){
+						prime(ns, cluster, s, AZP, ptarget.hostname, div, j);
+						j++;
+					}
+					ptarget.primed = true;
+					port3.write(JSON.stringify(ptarget.primed));
+					port3.read();	
 					//increased interval to ensure Probe updates the primed field sent to analyzer which is sent to primer
 					//this way we don't try an prime a primed server.
 					await ns.sleep(INTERVAL * 5);
