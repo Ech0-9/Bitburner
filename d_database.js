6d1bc49af1005f80c4d1ca5593dc9a197843d6f7
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-22 19:14:07 -0600

    Create d_database.js

---
+++ d_database.js
@@ -0,0 +1,65 @@
+import {allServers, getConfig, setConfig, getPserver} from "util.js";
+
+export async function main(ns){
+	const ALL_SERVERS = allServers(ns,true);
+	const port1 = ns.getPortHandle(1);
+	//need to be updated with any new pservers
+	let serverList = ALL_SERVERS.concat([]);
+	let clusterList = [];
+	let serverIndex = 0;
+	let cycle = 0;
+	let oldptarget = getConfig(ns).oldPriority;
+	let ptarget = getConfig(ns).priority.host;
+	let primed = getConfig(ns).priority.primed;
+	
+	
+	function threads(sv){
+		const reserved = getConfig(ns).reservedRAM.home;
+		if(sv == "home"){
+			return Math.floor((ns.getServerMaxRam(sv) - reserved - ns.getServerUsedRam(sv)) / 1.75);
+		}
+		else{
+			return Math.floor((ns.getServerMaxRam(sv) - ns.getServerUsedRam(sv)) / 1.75);
+		}
+	}
+	function allThreads(svs){
+		return svs.reduce((a,b) => {
+			return threads(a) + threads(b);	
+		}, 0);
+	}
+	function makeCluster(svs){
+		return svs.filter(s => {
+			if(ns.hasRootAccess(s) && ns.getServersRequiredHackingLevel(s) <= ns.getHackingLevel()) return s;		
+		});
+	}
+	function serverPost(i){
+		while(ns.peek(i) == "NULL PORT DATA"){
+			ns.writePort(i, clusterList[serverIndex]);
+			if(serverIndex + 1 != clusterList.length){
+				serverIndex++;
+			}
+			else{
+				serverIndex = 0;
+			}
+		}
+	}
+	while(true){
+		//update ptarget if changed
+		ptarget = getConfig(ns).priority.host;
+		if(ptarget != oldptarget){
+			await setConfig(ns, {"priority": {"primed": false}});
+			await setConfig(ns, {"oldPriority": ptarget});
+		}
+		//servers refresh every cycle through of the current cluster loops
+		if(cycle > clusterList.length){
+			serverList = allServers(ns, true);
+			clusterList = makeCluster(serverList);
+			cycle = 0;
+		}
+		
+		serverPost(1);
+		cycle++;
+		
+	}
+	
+}
