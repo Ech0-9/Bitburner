2274ebb3a98a440048741457ecdaf820d2dc32fa
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-23 21:36:23 -0600

    Update d_database.js

--- d_database.js
+++ d_database.js
@@ -1,86 +1,122 @@
-import {allServers, getConfig, setConfig} from "util.js";
+import { allServers, getConfig, setConfig } from "util.js";
 
-export async function main(ns){
-	const ALL_SERVERS = allServers(ns,true);
-	const port1 = ns.getPortHandle(1);
+export async function main(ns) {
+	const ALL_SERVERS = allServers(ns, true);
 	//need to be updated with any new pservers
 	let serverList = ALL_SERVERS.concat([]);
 	let clusterList = [];
 	let serverIndex = 0;
-	let cycle = 0;
 	let oldptarget = getConfig(ns).oldPriority;
 	let ptarget = getConfig(ns).priority.host;
-	let primed = getConfig(ns).priority.primed;
-	
-	
-	function threads(sv){
+
+
+	function threads(sv) {
 		const reserved = getConfig(ns).reservedRAM.home;
-		if(sv == "home"){
+		if (sv == "home") {
 			return Math.floor((ns.getServerMaxRam(sv) - reserved - ns.getServerUsedRam(sv)) / 1.75);
 		}
-		else{
+		else {
 			return Math.floor((ns.getServerMaxRam(sv) - ns.getServerUsedRam(sv)) / 1.75);
 		}
 	}
-	function allThreads(svs){
-		return svs.reduce((a,b) => {
-			return threads(a) + threads(b);	
-		}, 0);
-	}
-	function makeCluster(svs){
-		return svs.filter(s => {
-			if(ns.hasRootAccess(s) && ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel()) return s;		
+	function allThreads(svs) {
+		return svs.map(sv => {
+			return threads(sv);
+			}).reduce((a, b) => {
+			return a + b;
 		});
 	}
-	async function serverPost(i){
-		while(ns.peek(i) == "NULL PORT DATA"){
-			let cs = "";
+	function makeCluster(svs) {
+		return svs.filter(s => {
+			if (ns.hasRootAccess(s) && ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel()) return s;
+		});
+	}
+	async function serverPost(i, ty) {
+		ns.print(ty);
+		if (ns.peek(i) != "NULL PORT DATA") {
 			let found = false;
-			while(!found){
-				if(threads(clusterList[serverIndex]) > 0){
-					if(serverIndex + 1 != clusterList.length){
-						cs = clusterList[serverIndex];
-						serverIndex++;
-						found = true;	
-					}
-					else{
-						cs = clusterList[serverIndex];
-						serverIndex = 0;
+			let svs = [];
+			let type = ty;
+			let aRam = 0;
+			if (ns.peek(i) == "P" || ns.peek(i) == "B") {
+				type = ns.readPort(i);
+			}
+			ns.print(type);
+			let total = ns.peek(i);
+			for (let i = serverIndex, j = 0; j < clusterList.length; j++) {
+				let t = threads(clusterList[i]);
+				if (t > 0) {
+					total -= threads(clusterList[i]);
+					if (total < 0) {
+						svs.push(clusterList[i]);
+						serverIndex = i;
 						found = true;
-					}
-				}
-				else{
-					if(serverIndex + 1 != clusterList.length){
-						serverIndex++;	
-					}
-					else{
-						serverIndex = 0;
 						break;
 					}
+					else if (total == 0) {
+						svs.push(clusterList[i]);
+						serverIndex = i++;
+						found = true;
+						break;
+					}
+					else {
+						svs.push(clusterList[i]);
+						i++;
+					}
 				}
-						
+				else {
+					i++;
+				}
+				if (i > clusterList.length) {
+					i = 0;
+				}
+
 			}
-			if(cs != ""){
-				await ns.writePort(i, cs);
+			if (type == "P") {
+				aRam = allThreads(svs);
+				ns.print(`line 75 aRam: ${aRam}`);
+				svs.unshift(aRam);
+				await ns.writePort(i, JSON.stringify(svs));
+				ns.readPort(i);
+				type = "";
+				return type;
+			}
+			else {
+				if (found) {
+					aRam = allThreads(svs);
+					ns.print(`line 85 aRam: ${aRam}`);
+					svs.unshift(aRam);
+					await ns.writePort(i, JSON.stringify(svs));
+					ns.readPort(i);
+					type = "";
+					return type;
+				}
+				else {
+					return type;
+				}
 			}
 		}
 	}
-	while(true){
+	while (true) {
 		//update ptarget if changed
+		ns.print("start of data");
 		ptarget = getConfig(ns).priority.host;
-		if(ptarget != oldptarget){
-			await setConfig(ns, {"priority": {"primed": false}});
-			await setConfig(ns, {"oldPriority": ptarget});
+		let type = "";
+		if (ptarget != oldptarget) {
+			await setConfig(ns, { "priority": { "primed": false } });
+			await setConfig(ns, { "oldPriority": ptarget });
 		}
 		//servers refresh every cycle through of the current cluster loops
-		if(serverIndex == 0){
+		if (serverIndex == 0) {
 			serverList = allServers(ns, true);
 			clusterList = makeCluster(serverList);
 		}
-		
-		await serverPost(1);
+		ns.print("trying to find");
+		type = await serverPost(1, type);
+		ns.print(`result: ${type}`);
 		await ns.sleep(100);
-		
+		ns.print("endof data");
+
 	}
-	
+
 }
