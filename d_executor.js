820e0f2d92ca968cbdb7da4c783982d53d66380a
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-23 21:36:51 -0600

    Update d_executor.js

--- d_executor.js
+++ d_executor.js
@@ -1,36 +1,45 @@
-import { getConfig, setConfig } from "util.js";
+import { getConfig, setConfig, generateId } from "util.js";
 import { analyze } from "u_functions.js";
 export async function main(ns) {
-		const SCRIPTS = ["t_extract.js", "t_engorge.js", "t_enfeeble.js"];
-		let analysis = {};
-		let server = "";
-		let neededServers = [];
-		let batchId = 0;
-		let primed = false;
-		function threads(sv) {
-			const reserved = getConfig(ns).reservedRAM.home;
-			if (sv == "home") {
-				return Math.floor((ns.getServerMaxRam(sv) - reserved - ns.getServerUsedRam(sv)) / 1.75);
-			}
-			else {
-				return Math.floor((ns.getServerMaxRam(sv) - ns.getServerUsedRam(sv)) / 1.75);
-			}
+	const SCRIPTS = ["t_extract.js", "t_engorge.js", "t_enfeeble.js"];
+	let analysis = {};
+	let servs = [];
+	let servsBack = [];
+	let batchId = 0;
+	let primed = false;
+	function threads(sv) {
+		const reserved = getConfig(ns).reservedRAM.home;
+		if (sv == "home") {
+			return Math.floor((ns.getServerMaxRam(sv) - reserved - ns.getServerUsedRam(sv)) / 1.75);
 		}
-		async function serverQuery(i) {
-			while (ns.peek(i) == "NULL PORT DATA") {
-				await ns.sleep(10);
-			}
-			return ns.readPort(i);
+		else {
+			return Math.floor((ns.getServerMaxRam(sv) - ns.getServerUsedRam(sv)) / 1.75);
 		}
-		function hgw(ns, ty, s, hgwt, az, tg, id) {
-			const TDIF = 200;
+	}
+	async function serverQuery(i, rThreads, pb) {
+		while (ns.peek(i) == "EMPTY") {
+			await ns.sleep(100);
+		}
+		await ns.writePort(i, pb);
+		await ns.writePort(i, rThreads);
+		while (ns.peek(i) == rThreads || ns.peek(i) == pb) {
+			await ns.sleep(100);
+		}
+		return JSON.parse(ns.readPort(i));
+	}
+	function hgw(ty, svs, hgwt, az, tg, id) {
+		ns.print(svs);
+		const TDIF = 200;
+		if (svs.length != 0) {
+			let s = svs.shift();
 			let st = threads(s);
 			let d = 0;
 			let i = -1;
+			let nt = 0;
 			switch (ty) {
 				case "HACK":
 					i = 0;
-					d = az.weakTime - az.growTime - TDIF;
+					d = az.weakTime - az.hackTime - TDIF;
 					break;
 				case "GROW":
 					i = 1;
@@ -41,84 +50,73 @@ export async function main(ns) {
 					break;
 			}
 			let arrArgs = ["--target", tg, "--delay", d, "--id", id];
-			if (hgwt - st >= 0) {
+			if (hgwt - st == 0) {
 				ns.exec(SCRIPTS[i], s, st, ...arrArgs);
 			}
-			else {
+			else if (hgwt - st < 0) {
 				ns.exec(SCRIPTS[i], s, hgwt, ...arrArgs);
+				svs.unshift(s);
+			}
+			else {
+				nt = hgwt - st;
+				ns.print("new internal hgw");
+				hgw(ty, svs, nt, az, tg, id);
+				ns.print("back up a level");
+				ns.exec(SCRIPTS[i], s, st, ...arrArgs);
 
 			}
-			return hgwt - st;
 		}
-		while (true) {
-			primed = getConfig(ns).priority.primed;
-			ns.tprint(primed);
-			let ptarget = getConfig(ns).priority.host;
-			ns.tprint(ptarget);
-			if (!primed) {
-				ns.tprint("priming");
-				analysis = analyze(ns, ptarget, "P");
-				let g = analysis.growThreads;
-				let w = analysis.g_weakThreads;
-				server = await serverQuery(1);
-				while (g > 0 || w > 0) {
-					if (threads(server) < 1) {
-						server = await serverQuery(1);
-					}
-					if (g > 0) {
-						g = hgw(ns, "GROW", server, g, analysis, ptarget, 0);
-					}
-					else {
-						w = hgw(ns, "WEAK", server, w, analysis, ptarget, 0);
-					}
-				}
-				await setConfig(ns, { "priority": { "primed": true } });
+
+	}
+	function prime(s, az, tg, id, d) {
+		ns.print(`Starting Grow: ${s}`);
+		ns.print(`GrowThreads: ${az.growThreads / d}`)
+		hgw("GROW", s, Math.ceil(az.growThreads / d), az, tg, id);
+		ns.print(`Starting weak: ${s}`);
+		hgw("WEAK", s, Math.ceil(az.g_weakThreads / d), az, tg, id);
+	}
+	function batch(s, az, tg, id) {
+		hgw("HACK", s, az.hackThreads, az, tg, id);
+		hgw("WEAK", s, az.h_weakThreads, az, tg, id);
+		hgw("GROW", s, az.growThreads, az, tg, id);
+		hgw("WEAK", s, az.g_weakThreads, az, tg, id);
+	}
+	while (true) {
+		primed = getConfig(ns).priority.primed;
+		ns.print(primed);
+		let ptarget = getConfig(ns).priority.host;
+		ns.print(ptarget);
+		let total = 0;
+		let div = 0;
+		let aRam = 0;
+		if (!primed) {
+			analysis = analyze(ns, ptarget, "P");
+			ns.print(analysis);
+			total = analysis.total;
+			servs = await serverQuery(1, total, "P");
+			aRam = servs.shift();
+			ns.print(aRam);
+			servsBack = servs.concat([]);
+			if (total / aRam > 0) {
+				div = Math.ceil(total / aRam);
+				ns.print(div);
 			}
-			else {
-				ns.tprint("batching");
-				analysis = analyze(ns, ptarget, "B");
-				let total = 0;
-				if (ns.serverExists(neededServers[0])) {
-					total = threads(neededServers[0]);
-				}
-				let h = analysis.hackThreads;
-				let w1 = analysis.h_weakThreads;
-				let g = analysis.growThreads;
-				let w2 = analysis.g_weakThreads;
-				while (total < analysis.total) {
-					server = await serverQuery(1);
-					total += threads(server);
-					neededServers.push(server);
-				}
-				for (let i = 0; i < neededServers.length; i++) {
-					if (h > 0) {
-						h = hgw(ns, "HACK", neededServers[i], h, analysis, ptarget, batchId);
-						if (h >= 0) continue;
-					}
-					if (w1 > 0) {
-						w1 = hgw(ns, "WEAK", neededServers[i], w1, analysis, ptarget, batchId);
-						if (w1 >= 0) continue;
-					}
-					if (g > 0) {
-						g = hgw(ns, "GROW", neededServers[i], g, analysis, ptarget, batchId);
-						if (g >= 0) continue;
-					}
-					if (w2 > 0) {
-						w2 = hgw(ns, "WEAK", neededServers[i], w2, analysis, ptarget, batchId);
-						if (w2 >= 0) continue;
-					}
-				}
-				//neededServers clean up
-				if (threads(neededServers[neededServers.length - 1]) > 0) {
-					while (neededServers.length != 1) {
-						neededServers.shift();
-					}
-				}
-				else {
-					neededServers = [];
-				}
+			for (let i = 0; i < div; i++) {
+				prime(servs, analysis, ptarget, 0, div);
+				servs = servsBack.concat([]);
 			}
-			await ns.sleep(250);
+			await setConfig(ns, { "priority": { "primed": true } });
 		}
-	
+		else {
+			analysis = analyze(ns, ptarget, "B");
+			total = analysis.total;
+			servs = await serverQuery(1, total, "B");
+			aRam = servs.shift();
+			batchId = generateId(16);
+			batch(serv, analysis, ptarget, batchId)
+
+		}
+		await ns.sleep(250);
+	}
+
 }
