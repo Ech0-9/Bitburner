40c19ba1f81581d73ccf028330e4556124e1604f
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-29 20:20:37 -0600

    Update t_enfeeble.js

--- t_enfeeble.js
+++ t_enfeeble.js
@@ -1,13 +1,16 @@
 /** @Param {NS} ns **/
+import {getConfig} from "util.js";
 export async function main(ns){
 	const f = ns.flags([
-		["target", ""],
-		["delay", ""],
+		["host", ""],
+		["batchNum", 1],
 		["id", -1]
 	]);
+	const INTERVAL = getConfig(ns).interval;
 	let target = "";
 	while(true){
-		target = ns.read("TARGET.txt");
+		target = ns.read(`TARGET_${f.host}.txt`);
 		await ns.weaken(target);
+		await ns.sleep(INTERVAL * 4 * f.batchNum);
 	}
 }
