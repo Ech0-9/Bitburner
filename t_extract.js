681363812a9cfe21558fc0d310eee0f9fb639e74
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-29 19:17:56 -0600

    Update t_extract.js

--- t_extract.js
+++ t_extract.js
@@ -5,7 +5,21 @@ export async function main(ns){
 		["delay", ""],
 		["id", -1]
 	]);
-	await ns.sleep(f.delay);
-	let result =  ns.hack(f.target);
-	ns.toast(`Hack Result on ${f.target}: ${result}`, "success", 3000);
+	const TDIF = 200;
+	let target = "";
+	let oldTarget = "";
+	let delay = 0;
+	while(true){
+		target = ns.read("TARGET.txt");
+		if(target != oldTarget){
+			await ns.write("Profit.txt", 0, "w");
+			oldTarget = target;
+		}
+		delay = ns.getWeakenTime(target) - ns.getHackTime(target) - TDIF;
+		await ns.sleep(delay);
+		let result =  ns.hack(target);
+		let total = Number(ns.read("Profit.txt"));
+		await ns.write("Profit.txt", total + result, "w");
+		//ns.toast(`Hack Result on ${f.target}: ${result}`, "success", 3000);
+	}
 }
