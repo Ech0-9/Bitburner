d53ce6305a3577bff741ea6a12e58de5ecd98a45
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-29 20:01:52 -0600

    Update t_extract.js

--- t_extract.js
+++ t_extract.js
@@ -1,25 +1,24 @@
 //will be the hack script
 export async function main(ns){
 	const f = ns.flags([
-		["target", ""],
-		["delay", ""],
+		["host", ""],
 		["id", -1]
 	]);
 	const TDIF = 200;
 	let target = "";
-	let oldTarget = "";
+	//let oldTarget = "";
 	let delay = 0;
 	while(true){
-		target = ns.read("TARGET.txt");
-		if(target != oldTarget){
+		target = ns.read(`TARGET_${f.host}.txt`);
+		/*if(target != oldTarget){
 			await ns.write("Profit.txt", 0, "w");
 			oldTarget = target;
-		}
+		}*/
 		delay = ns.getWeakenTime(target) - ns.getHackTime(target) - TDIF;
 		await ns.sleep(delay);
 		let result =  ns.hack(target);
-		let total = Number(ns.read("Profit.txt"));
-		await ns.write("Profit.txt", total + result, "w");
+		/*let total = Number(ns.read("Profit.txt"));
+		await ns.write("Profit.txt", total + result, "w");*/
 		//ns.toast(`Hack Result on ${f.target}: ${result}`, "success", 3000);
 	}
 }
