a5fd056253a89215ef5b7817d91975d56c465ad4
Jdmorris <61710689+Ech0-9@users.noreply.github.com>
2022-01-29 20:17:07 -0600

    Update t_engorge.js

--- t_engorge.js
+++ t_engorge.js
@@ -1,17 +1,20 @@
 /** @param {NS} ns **/
+import {getConfig} from "util.js";
 export async function main(ns){
 	const f = ns.flags([
-		["target", ""],
-		["delay", ""],
+		["host", ""],
+		["batchNum", 0],
 		["id", -1]
 	]);
-	const TDIF = 200;
+	const INTERVAL = getConfig(ns).interval;
+	//const TDIF = 200;
 	let target = "";
 	let delay = 0;
 	while(true){
-		target = ns.read("TARGET.txt");
-		delay = ns.getWeakenTime(target) - ns.getGrowTime(target) - TDIF;
+		target = ns.read(`TARGET_${f.host}.txt`);
+		delay = ns.getWeakenTime(target) - ns.getGrowTime(target);
 		await ns.sleep(delay);
 		await ns.grow(target);
+		await ns.sleep(INTERVAL * 4 * f.batchNum);
 	}
 }
